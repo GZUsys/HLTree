b0b0096eb8ed29b5a8e315b2e5be8764bbed89e8
GUhexy <106083317+GUhexy@users.noreply.github.com>
2024-05-01 20:44:56 +0800

    Delete fast_fair directory

--- fast_fair/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-g++ -O3 -std=c++11 -DNDEBUG -DMUTEX -m64 -D_REENTRANT -fno-strict-aliasing -I./atomic_ops -DINTEL -Wno-unused-value -Wno-format -o ./main-gu-zipfian main-gu-zipfian.c -m64 -lpmemobj -lpmem -lpthread 
-#-I/root/install/pcm/src -L/root/install/pcm -lpcm
-#-lprofiler
-#-I/root/install/pcm/src -L/root/install/pcm
